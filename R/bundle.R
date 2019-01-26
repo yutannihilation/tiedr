d7ca2761c4199d6ea55a485191a1141661a1b4fb
Hiroaki Yutani <yutani.ini@gmail.com>
2019-01-26 15:14:22 +0900

    Reorder columns

--- R/bundle.R
+++ R/bundle.R
@@ -7,5 +7,26 @@ bundle <- function(data, ..., .key = "data") {
 
   out <- dplyr::select(data, !!! rlang::syms(group_vars))
   out[[.key]] <- dplyr::select(data, !!! rlang::syms(bundle_vars))
-  out
+  
+  out[relocate_cols(all_vars, !! .key := bundle_vars)]
+}
+
+relocate_cols <- function(orig_vars, ...) {
+  bundled <- rlang::list2(...)
+  if (any(!rlang::have_name(bundled))) {
+    rlang::abort("All ... must be named")
+  }
+
+  rest <- setdiff(orig_vars, purrr::flatten_chr(bundled))
+
+  # insert to the position of the left-most column
+  bundled_orders <- purrr::map_int(bundled, ~ min(match(., orig_vars)))
+  rest_orders <- match(rest, orig_vars)
+  names(rest_orders) <- rest
+
+  if (any(is.na(bundled_orders)) || any(is.na(bundled_orders))) {
+    rlang::abort("Some variable are not found in orig_vars")
+  }
+
+  names(sort(c(bundled_orders, rest_orders)))
 }
--- tests/testthat/test-bundle.R
+++ tests/testthat/test-bundle.R
@@ -1,13 +1,21 @@
 context("test-bundle")
 
 test_that("bundle() for simple cases works", {
-  d <- data.frame(x = 1:3, a1 = 11:13, a2 = 21:23)
+  d <- data.frame(x = 1:3, a1 = 11:13, y = 1:3, a2 = 21:23)
 
-  expected <- d[, c("x"), drop = FALSE]
+  expected <- d[, c("x", "y"), drop = FALSE]
   expected$data <- d[, c("a1", "a2")]
+  expected <- expected[, c("x", "data", "y")]
 
   expect_equal(bundle(d, a1, a2), expected)
   expect_equal(bundle(d, c("a1", "a2")), expected)
-  expect_equal(bundle(d, -x), expected)
+  expect_equal(bundle(d, -x, -y), expected)
   expect_equal(bundle(d, starts_with("a")), expected)
+})
+
+test_that("relocate_cols() works", {
+  expect_equal(relocate_cols(c("x", "a1", "y", "a2"), a = c("a1", "a2")), c("x", "a", "y"))
+  expect_equal(relocate_cols(c("x", "a1", "y", "a2"), !!! list(a = c("a1", "a2"))), c("x", "a", "y"))
+
+  expect_equal(relocate_cols(c("x", "a1", "b1", "y", "b2", "a2"), a = c("a1", "a2"), b = c("b1", "b2")), c("x", "a", "b", "y"))
 })
