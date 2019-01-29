c3128b7b42041256c5775de64c6055d1b8089e17
Hiroaki Yutani <yutani.ini@gmail.com>
2019-01-30 08:10:42 +0900

    Implement flatten_names_depth()

--- R/utils.R
+++ R/utils.R
@@ -4,15 +4,31 @@ append_cols <- function(.data, ..., offset = ncol(.data)) {
   tibble::tibble(!!!.data[seq_len(offset)], ..., !!!.data[seq_len(n_col - offset) + offset])
 }
 
-flatten_chr_depth <- function(.x, .depth = purrr::vec_depth(.x) - 1) {
-  if (.depth <= 0) 
+flatten_names_depth <- function(.x, .depth = purrr::vec_depth(.x) - 1) {
+  res <- flatten_names_depth_rec(.x, .depth = .depth)
+  sort(unique(res))
+}
+
+flatten_names_depth_rec <- function(.x, .depth = purrr::vec_depth(.x) - 1) {
+  if (.depth < 0) 
     rlang::abort("Invalid depth")
   
-  if (.depth == 1) 
-    return(purrr::flatten_chr(.x))
+  if (.depth == 0) {
+    if (is.character(.x)) {
+      return(.x)
+    }
+    
+    res <- names(.x)
+    
+    if (is.null(res)) {
+      rlang::abort("The elements are not characters and doesn't have names.")
+    }
+    
+    return(res)
+  }
   
   res <- purrr::map(.x, function(x) {
-    flatten_chr_depth(x, .depth - 1)
+    flatten_names_depth_rec(x, .depth - 1)
   })
   
   purrr::flatten_chr(res)
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,1 +1,10 @@
 context("test-utils")
+
+test_that("flatten_names_depth() works", {
+  expect_equal(flatten_names_depth(c("a", "b")), c("a", "b"))
+  expect_equal(flatten_names_depth(list(A = c("a", "b"), B = c("a", "c")), 0), c("A", "B"))
+  expect_equal(flatten_names_depth(list(A = list(y = c("a", "c")), B = list(x = c("b"))), 2), c("a", "b", "c"))
+  expect_equal(flatten_names_depth(list(A = list(y = c("a", "c")), B = list(x = c("b"))), 1), c("x", "y"))
+  expect_equal(flatten_names_depth(list(A = list(y = c("a", "c")), B = list(x = c("b"))), 0), c("A", "B"))
+  expect_error(flatten_names_depth(list(a = 1:2, b = "x"), 1))
+})
